8b4e33afd843879d7e217f9f84feb8b2ccd57984
OKmeray <yar.rom.rom@gmail.com>
2023-07-19 13:35:41 +0300

    Fixed main

--- main.js
+++ main.js
@@ -153,7 +153,7 @@ const catalogValues = {
 
 // translateDynamic(); not an optimization
 
-if (document.URL.includes("catalog-en")) {
+if (document.URL.includes("eng")) {
     console.log(document.URL);
     translateToEN();
 }
